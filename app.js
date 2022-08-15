db79e43a0ebf0e3a036372d41d7585e952126f96
Matias Jacinto <matymdq2015@gmail.com>
2022-08-15 20:34:01 -0300

    instalacion, mysql,util,md5-creacion modelUsuarios

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var electrodomesticosRouter = require("./routes/electrodomesticos");
 var hogarRouter = require("./routes/hogar");
 var herramientasRouter = require("./routes/herramientas");
 var contactoRouter = require("./routes/contacto");
+var loginRouter = require("./routes/admin/login");
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use("/electrodomesticos", electrodomesticosRouter);
 app.use("/hogar", hogarRouter);
 app.use("/herramientas", herramientasRouter);
 app.use("/contacto", contactoRouter);
+app.use("/admin/login", loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
