a2af3dd3f24599d5df5bbac250afaf9024da9c5f
Matias Jacinto <matymdq2015@gmail.com>
2022-08-24 22:26:19 -0300

    CRUD de ofertas, inst cloudinary, express-fileuuload,css

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var router = express.Router();
-
+var ofertasModels = require("../models/ofertasModels");
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { isHome: true });
+router.get("/", async function (req, res, next) {
+  var ofertas = await ofertasModels.getOfertas();
+  res.render("index", {
+    isHome: true,
+    ofertas,
+  });
 });
 
 module.exports = router;
